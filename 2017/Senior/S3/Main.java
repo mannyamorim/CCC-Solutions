4860174825d04c8dd8ef9cd99eac1291aa1c65f2
Emmanuel Mathi-Amorim <manny@amorim.ca>
2017-03-06 17:00:10 -0500

    Added CCC 2017 S3 in C++ and Java.

---
+++ 2017/Senior/S3/Main.java
@@ -0,0 +1,90 @@
+// CCC 2016 Senior 3
+// Solution by Emmanuel Mathi-Amorim
+
+import java.util.Scanner;
+
+public class Main
+{
+	static int n;
+	static int[] countingSort = new int[2001];
+	static int[] lengths = new int[2001];
+
+	static void read_input()
+	{
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		for (int i = 0; i < 2001; i++)
+		{
+			countingSort[i] = 0;
+		}
+
+		for (int i = 0; i < n; i++)
+		{
+			int l = sc.nextInt();
+			countingSort[l]++;
+		}
+	}
+
+	static int find_number_of_boards_at_height(int height)
+	{
+		for (int i = 0; i < 2001; i++)
+		{
+			lengths[i] = countingSort[i];
+		}
+
+		int a = Math.max(1, height - 2000);
+		int b = height - a;
+
+		int num_of_boards = 0;
+
+		while (a <= (height / 2))
+		{
+			if (a == b)
+			{
+				int boards = lengths[a] / 2;
+				lengths[a] -= boards * 2;
+
+				num_of_boards += boards;
+			}
+			else
+			{
+				int boards = Math.min(lengths[a], lengths[b]);
+				lengths[a] -= boards;
+				lengths[b] -= boards;
+
+				num_of_boards += boards;
+			}
+
+			++a;
+			--b;
+		}
+
+		return num_of_boards;
+	}
+
+	public static void main(String[] args)
+	{
+		read_input();
+
+		int max_boards = 0;
+		int heights_at_max_boards = 0;
+
+		for (int i = 2; i <= 4000; i++)
+		{
+			int boards = find_number_of_boards_at_height(i);
+
+			if (boards > max_boards)
+			{
+				max_boards = boards;
+				heights_at_max_boards = 0;
+			}
+			if (boards == max_boards)
+			{
+				heights_at_max_boards++;
+			}
+		}
+
+		System.out.println(max_boards + " " + heights_at_max_boards);
+	}
+}
---
+++ 2017/Senior/S3/S3.cpp
@@ -0,0 +1,86 @@
+// CCC 2016 Senior 3
+// Solution by Emmanuel Mathi-Amorim
+
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+
+int n;
+int countingSort[2001];
+int lengths[2001];
+
+void read_input()
+{
+	std::cin >> n;
+
+	memset(countingSort, 0, sizeof(countingSort));
+
+	for (int i = 0; i < n; i++)
+	{
+		int l;
+		std::cin >> l;
+		countingSort[l]++;
+	}
+}
+
+int find_number_of_boards_at_height(int height)
+{
+	memcpy(lengths, countingSort, sizeof(lengths));
+
+	int a = std::max(1, height - 2000);
+	int b = height - a;
+
+	int num_of_boards = 0;
+
+	while (a <= (height / 2))
+	{
+		if (a == b)
+		{
+			int boards = lengths[a] / 2;
+			lengths[a] -= boards * 2;
+
+			num_of_boards += boards;
+		}
+		else
+		{
+			int boards = std::min(lengths[a], lengths[b]);
+			lengths[a] -= boards;
+			lengths[b] -= boards;
+
+			num_of_boards += boards;
+		}
+
+		++a;
+		--b;
+	}
+
+	return num_of_boards;
+}
+
+int main()
+{
+	read_input();
+
+	int max_boards = 0;
+	int heights_at_max_boards = 0;
+
+	for (int i = 2; i <= 4000; i++)
+	{
+		int boards = find_number_of_boards_at_height(i);
+
+		if (boards > max_boards)
+		{
+			max_boards = boards;
+			heights_at_max_boards = 0;
+		}
+		if (boards == max_boards)
+		{
+			heights_at_max_boards++;
+		}
+	}
+
+	std::cout << max_boards << " " << heights_at_max_boards;
+	std::cin >> n;
+
+    return 0;
+}
