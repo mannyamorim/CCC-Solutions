9bace99dbde13d848179bb2d52ad7c4c39b73621
Tobias <TobCar@users.noreply.github.com>
2017-06-24 22:18:54 -0700

    Refractor for readability
    Made variable names more descriptive and removed extra indentation.

--- 2017/Senior/S3/Main.java
+++ 2017/Senior/S3/Main.java
@@ -1,90 +1,93 @@
 // CCC 2017 Senior 3
 // Solution by Emmanuel Mathi-Amorim
+// Refractoring by Tobias Carryer
 
 import java.util.Scanner;
 
 public class Main
 {
-	static int n;
-	static int[] countingSort = new int[2001];
-	static int[] lengths = new int[2001];
+    final static int maxWoodLength = 2000;
 
-	static void read_input()
-	{
-		Scanner sc = new Scanner(System.in);
-		n = sc.nextInt();
+    static int[] amountOf = new int[maxWoodLength+1];
 
-		for (int i = 0; i < 2001; i++)
-		{
-			countingSort[i] = 0;
-		}
+    static void readInput()
+    {
+        Scanner sc = new Scanner(System.in);
+        int numOfWood = sc.nextInt();
 
-		for (int i = 0; i < n; i++)
-		{
-			int l = sc.nextInt();
-			countingSort[l]++;
-		}
-	}
+        for (int i = 0; i < 2001; i++)
+        {
+            amountOf[i] = 0;
+        }
 
-	static int find_number_of_boards_at_height(int height)
-	{
-		for (int i = 0; i < 2001; i++)
-		{
-			lengths[i] = countingSort[i];
-		}
+        for( int i = 0; i < numOfWood; i++ )
+        {
+            int length = sc.nextInt();
+            amountOf[length]++;
+        }
+    }
 
-		int a = Math.max(1, height - 2000);
-		int b = height - a;
+    static int findNumberOfBoardsAtHeight( int height )
+    {
+        int[] amountOfCopy = new int[amountOf.length];
 
-		int num_of_boards = 0;
+        for( int i = 0; i < amountOf.length; i++ )
+        {
+            amountOfCopy[i] = amountOf[i];
+        }
 
-		while (a <= (height / 2))
-		{
-			if (a == b)
-			{
-				int boards = lengths[a] / 2;
-				lengths[a] -= boards * 2;
+        int a = Math.max(1, height - maxWoodLength);
+        int b = height - a;
 
-				num_of_boards += boards;
-			}
-			else
-			{
-				int boards = Math.min(lengths[a], lengths[b]);
-				lengths[a] -= boards;
-				lengths[b] -= boards;
+        int numOfBoards = 0;
 
-				num_of_boards += boards;
-			}
+        while( a <= (height / 2) )
+        {
+            if (a == b)
+            {
+                int boards = amountOfCopy[a] / 2;
+                amountOfCopy[a] -= boards * 2;
 
-			++a;
-			--b;
-		}
+                numOfBoards += boards;
+            }
+            else
+            {
+                int boards = Math.min(amountOfCopy[a], amountOfCopy[b]);
+                amountOfCopy[a] -= boards;
+                amountOfCopy[b] -= boards;
 
-		return num_of_boards;
-	}
+                numOfBoards += boards;
+            }
 
-	public static void main(String[] args)
-	{
-		read_input();
+            ++a;
+            --b;
+        }
 
-		int max_boards = 0;
-		int heights_at_max_boards = 0;
+        return numOfBoards;
+    }
 
-		for (int i = 2; i <= 4000; i++)
-		{
-			int boards = find_number_of_boards_at_height(i);
+    public static void main(String[] args)
+    {
+        readInput();
 
-			if (boards > max_boards)
-			{
-				max_boards = boards;
-				heights_at_max_boards = 0;
-			}
-			if (boards == max_boards)
-			{
-				heights_at_max_boards++;
-			}
-		}
+        int maxBoards = 0;
+        int heightAtMaxBoards = 0;
 
-		System.out.println(max_boards + " " + heights_at_max_boards);
-	}
+        for( int i = 2; i <= 4000; i++ )
+        {
+            int boards = findNumberOfBoardsAtHeight(i);
+
+            if( boards > maxBoards )
+            {
+                maxBoards = boards;
+                heightAtMaxBoards = 0;
+            }
+            if( boards == maxBoards )
+            {
+                heightAtMaxBoards++;
+            }
+        }
+
+        System.out.println(maxBoards + " " + heightAtMaxBoards);
+    }
 }
