588238f3289553e8a3da483944b3398e8290ea0d
Emmanuel Mathi-Amorim <manny@amorim.ca>
2020-04-13 21:01:28 -0400

    2020-S3: Improved hashing speed

--- 2020/senior/s3.cpp
+++ 2020/senior/s3.cpp
@@ -34,6 +34,28 @@ void compute_hist() {
 		hist[needle[i] - 'a']++;
 }
 
+class pre_hashed_string_view {
+public:
+	pre_hashed_string_view(const char *str, int n, size_t hash) : str(str), n(n), hash(hash) {}
+	bool operator ==(const pre_hashed_string_view &b) const {
+		if (n != b.n)
+			return false;
+		return strncmp(str, b.str, n) == 0;
+	}
+
+	const char *str;
+	int n;
+	size_t hash;
+};
+
+namespace std {
+	template<> struct hash<pre_hashed_string_view> {
+		size_t operator()(pre_hashed_string_view const& obj) const noexcept {
+			return obj.hash;
+		}
+	};
+}
+
 /*
  * We compute a histogram of each of the possible substring positions
  * and compare with the original histogram of the needle string.
@@ -43,25 +65,39 @@ void compute_hist() {
 int solve() {
 	int running_hist[NUM_ALPHABET] = { 0 };
 
-	/* compute histogram for the first position */
-	for (int i = 0; i < n; i++)
-		running_hist[haystack[i] - 'a']++;
+	const int prime = 2000003;
+	int power = 1;
+	int hash = 0;
 
-	std::unordered_set<std::string> permutations;
+	for (int i = 0; i < n - 1; i++)
+		power = (power * NUM_ALPHABET) % prime;
+
+	/* compute histogram for the first position */
+	for (int i = 0; i < n; i++) {
+		running_hist[haystack[i] - 'a']++;
+		hash = (NUM_ALPHABET * hash + (haystack[i] - 'a' + 1)) % prime;
+	}
+
+	std::unordered_set<pre_hashed_string_view> permutations;
 
 	/* check the first position against the original histogram */
 	if (memcmp(hist, running_hist, sizeof(int) * NUM_ALPHABET) == 0)
-		permutations.emplace(haystack, n);
+		permutations.emplace(haystack, n, hash);
 
 	/* iterate through the remaining positions */
 	for (int i = 0; i < h - n; i++) {
 		/* update the histogram */
 		running_hist[haystack[i]     - 'a']--;
 		running_hist[haystack[i + n] - 'a']++;
-		
+
+		/* update the hash */
+		hash = (NUM_ALPHABET * (hash - ((haystack[i] - 'a' + 1) * power)) + (haystack[i + n] - 'a' + 1)) % prime;
+		if (hash < 0)
+			hash = hash + prime;
+
 		/* check the position */
 		if (memcmp(hist, running_hist, sizeof(int) * NUM_ALPHABET) == 0)
-			permutations.emplace(&haystack[i + 1], n);
+			permutations.emplace(&haystack[i + 1], n, hash);
 	}
 
 	return permutations.size();
